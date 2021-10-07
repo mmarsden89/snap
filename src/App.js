a3f77d3e2da23eb6fa2d096c33538e3b7a5e6b89
mmarsden89 <mattmarsdendev@gmail.com>
2021-10-07 17:02:54 -0400

    add depreciated warning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./App.css";
+import "./App.scss";
 import Terminal from "./Components/Terminal/Terminal.js";
 
 function App() {
   return (
     <div className="App">
       <Terminal />
+      <div className="depreciated">
+        Snap has been depreciated. Thanks for checking it out!
+      </div>
     </div>
   );
 }
